e0c835315a20f2c58722b96121199e1b36741a59
Aymeric Rabot <aymeric@ledgerinvesting.com>
2019-08-16 16:13:43 -0400

    app.use(express.json())

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const https = require('https');
 const port = process.env.PORT || 8080;
+
+app.use(express.json());
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 const server = http.createServer(app);
